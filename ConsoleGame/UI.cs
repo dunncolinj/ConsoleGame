7f8a8176fae6a48ff0f779a5b39851f4d8b9cdfc
Colin Dunn <dunncolinj@gmail.com>
2021-10-09 14:07:51 -0400

    Add project files.

---
+++ ConsoleGame/UI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame
+{
+    class UI
+    {
+        // class for user interface
+    
+        // display "Hangman" title screen
+        // prompt user to select difficulty level
+        
+        // start game with appropriate difficulty level
+
+        // main game loop
+
+            // display hanging apparatus
+        
+            // display body parts
+
+            // display word to guess with letters blanked out
+
+            // display letters not already guessed
+
+            // get letter from user
+
+            // if letter in word, display letter(s) in position
+
+            // if all letters in word guessed, game ends and player wins
+
+            // if letter not in word, add one more body part
+
+            // if all body parts drawn (head, neck, left arm, right arm, torso, left leg, right leg) - game ends and player loses
+
+
+    
+    
+    }
+}
