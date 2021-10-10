96f6e23dddd50337c945d71615d9392d506143cb
Colin Dunn <dunncolinj@gmail.com>
2021-10-10 13:02:25 -0400

    20211010 - first session

--- ConsoleGame/Repository.cs
+++ ConsoleGame/Repository.cs
@@ -10,8 +10,121 @@ namespace ConsoleGame
 
     class Repository
     {
-    }
+
+        public List<string> _EasyWords
+        {
+            get
+            {
+                return _EasyWords;
+            }
+            set
+            {
+                
+            }
+        }
+
+        public List<string> _MediumWords
+        {
+            get
+            {
+                return _MediumWords;
+            }
+            set
+            {
+
+            }
+        }
+
+        public List<string> _HardWords
+        {
+            get
+            {
+                return _HardWords;
+            }
+            set
+            {
+
+            }
+        }
 
 
+        public int getDifficulty()
+        {
+            bool validInput = false;
+            int Difficulty;
 
-}
+            do
+            {
+                Console.WriteLine("Choose difficulty level: 1) easy, 2) medium, 3) hard");
+                char response = Console.ReadKey().KeyChar;
+                if (response == '1' || response == '2' || response == '3')
+                {
+                    validInput = true;
+                }
+
+                switch (response)
+                {
+                    case '1': Difficulty = 1;
+                        break;
+                    case '2': Difficulty = 2;
+                        break;
+                    case '3': Difficulty = 3;
+                        break;
+                    default: Difficulty = 0;
+                        break;
+                }
+            }
+            while (validInput == false);
+
+            return Difficulty;
+        }
+
+
+        public char getLetter()
+        {
+            bool validInput = false;
+            char response;
+            do
+            {
+                Console.WriteLine("Select a letter out of the alphabet:");
+                response = Console.ReadKey().KeyChar;
+                if (response >= 'a' || response >= 'z')
+                {
+                    validInput = true;
+                }
+            }
+            while (validInput == false);
+            return response;
+        }
+
+        public string GetWord(int difficulty)
+        {
+            string word;
+            int num;
+
+            switch (difficulty)
+            {
+                case 1: num = Random.Next(_EasyWords.Count());
+                    word = _EasyWords[num];
+                    break;
+                case 2: num = Random.Next(_MediumWords.Count());
+                    word = _MediumWords[num];
+                    break;
+                case 3: num = Random.Next(_HardWords.Count());
+                    word = _HardWords[num];
+                    break;
+                default: num = 0;
+                    word = "";
+                    break;
+            }
+            return word;
+        }
+
+
+        public void DisplayHangman(int bodyParts)
+        {
+            // display hanging apparatus
+            // display body parts
+        }
+    } // class
+} // namespace
--- ConsoleGame/UI.cs
+++ ConsoleGame/UI.cs
@@ -9,34 +9,56 @@ namespace ConsoleGame
     class UI
     {
         // class for user interface
-    
-        // display "Hangman" title screen
-        // prompt user to select difficulty level
-        
-        // start game with appropriate difficulty level
 
-        // main game loop
+        public void Game()
+        {
+            // display "Hangman" title screen
 
-            // display hanging apparatus
-        
-            // display body parts
+            // prompt user to select difficulty level
+            Console.Clear();
+            int Difficulty = getDifficulty();
 
-            // display word to guess with letters blanked out
-
-            // display letters not already guessed
-
-            // get letter from user
-
-            // if letter in word, display letter(s) in position
-
-            // if all letters in word guessed, game ends and player wins
-
-            // if letter not in word, add one more body part
-
-            // if all body parts drawn (head, neck, left arm, right arm, torso, left leg, right leg) - game ends and player loses
+            // start game with appropriate difficulty level
+            int bodyParts = 0;
+            string word = GetWord(Difficulty);
+            bool[] charguessed = new bool[word.Length];
+            bool[] letterused = new bool[26];
 
 
-    
-    
+            // main game loop
+            while (bodyParts < 7 )
+            {
+                Console.Clear();
+                DisplayHangman(bodyParts);
+
+                // display word to guess with letters blanked out
+
+                // display letters not already guessed
+
+                // get letter from user
+                char letter = getLetter();
+
+                // if all letters in word guessed, game ends and player wins
+
+                // if letter not in word, add one more body part
+
+                // if all body parts drawn (head, neck, left arm, right arm, torso, left leg, right leg) - game ends and player loses
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
     }
 }
